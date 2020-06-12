0252a77f9306247d1fa032233f8ff889a7baec8e
Abdul Razak <sendmesymbols@gmail.com>
2020-06-12 12:06:23 +0500

    Added 1x project in Level 1

---
+++ Level2/ContactBook/Database/db.sql
@@ -0,0 +1,17 @@
+create TABLE contact_users (
+	 id int(100) auto_increment primary key,
+	 username varchar(100),
+	 email varchar(100),
+	 password varchar(100)
+);
+
+
+create TABLE add_new (
+	id int(10) auto_increment primary key,
+	phone_no1 int(11),	
+	name varchar(20),
+	email varchar(50),	
+	phone_no2 varchar(50),
+	company varchar(90),
+	group_name varchar(20)
+);
---
+++ Level2/ContactBook/README.md
@@ -0,0 +1,5 @@
+# ContactBook
+##This is Contact Book project using Flask-MySql, Run .sql file against Db and chnage connection string.
+
+
+
